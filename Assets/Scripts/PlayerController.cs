1d12e245675034d7bdc43cc5e88374cea8c90ebd
David Wu <dawncoming3@gmail.com>
2024-04-12 09:09:52 -0400

    Added Character Movement and FPS Camera
    Character can move around the map, WASD to move. Spacebar to jump. Left Shift to sprint. First person camera also is added to look around with the mouse.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 // https://www.youtube.com/watch?v=qQLvcS9FxnY
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    public Camera playerCamera;
+    [SerializeField] float walkSpeed = 8f;
+    [SerializeField] float sprintSpeed = 12f;
+    private float jumpPower = 7f;
+    private float gravity = 14f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+
+    Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+
+    private bool canMove = true;
+    CharacterController characterController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Handles character movement
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to sprint
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeedX = canMove ? (isRunning ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float currentSpeedY = canMove ? (isRunning ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * currentSpeedX) + (right * currentSpeedY);
+
+        // Spacebar to jump
+        if (Input.GetKey(KeyCode.Space) && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Player rotation and camera movement
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
